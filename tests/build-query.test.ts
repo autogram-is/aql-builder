fba4e2f358885e0e832d62d5ddbc68b39db6e2a6
Jeff Eaton <jeff@eaton.fyi>
2023-02-09 01:32:31 -0600

    Genericizing test data

--- tests/build-query.test.ts
+++ tests/build-query.test.ts
@@ -3,26 +3,26 @@ import { Query } from '../src/query.js';
 
 test('query renders correctly', t => {
   const rendered = `
-  FOR item IN resources
-  FILTER item.parsed.domain IN @value0
+  FOR item IN responses
+  FILTER item.url.domain IN @value0
   COLLECT
-    code = item.code,
+    status = item.status,
     mime = item.mime
   WITH COUNT INTO total
-  FILTER code IN @value1
+  FILTER status IN @value1
   RETURN {
-    code,
+    status,
     mime,
     total
   }`;
 
-  const q = new Query('resources')
-    .filter('parsed.domain', ['schwab.com', 'tdameritrade'])
-    .groupBy('code')
+  const q = new Query('responses')
+    .filter('url.domain', ['example.com', 'test.com'])
+    .groupBy('status')
     .groupBy('mime')
     .count('total')
-    .filter('code', [200, 404])
-    .sort('parsed.domain', 'asc')
+    .filter('status', [200, 404])
+    .sort('url.domain', 'asc')
     .sort('total', 'asc')
     .build();
 
@@ -33,24 +33,24 @@ test('query renders correctly', t => {
 });
 
 test('spec and fluent match', t => {
-  const q = new Query('resources')
-    .filter('parsed.domain', ['schwab.com', 'tdameritrade'])
-    .groupBy('code')
+  const q = new Query('responses')
+    .filter('url.domain', ['example.com', 'test.com'])
+    .groupBy('status')
     .groupBy('mime')
     .count('total')
-    .filter('code', [200, 404])
-    .sort('parsed.domain', 'asc')
+    .filter('status', [200, 404])
+    .sort('url.domain', 'asc')
     .sort('total', 'asc')
     .build();
 
   const q2 = new Query({
-    collection: 'resources',
+    collection: 'responses',
     filters: [
-      { property: 'parsed.domain', in: ['schwab.com', 'tdameritrade'] },
-      { property: 'code', in: [200, 404], collected: true },
+      { property: 'url.domain', in: ['example.com', 'test.com'] },
+      { property: 'status', in: [200, 404], collected: true },
     ],
     aggregates: [
-      { property: 'code', aggregate: 'collect' },
+      { property: 'status', aggregate: 'collect' },
       { property: 'mime', aggregate: 'collect' },
     ],
     count: 'total'
