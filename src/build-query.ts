8d975036a5dbbeb16040d5334f300363f6e4aa86
Jeff Eaton <jeff@eaton.fyi>
2023-04-12 15:56:59 -0500

    Actually fixing it.

--- src/build-query.ts
+++ src/build-query.ts
@@ -316,9 +316,9 @@ function wrapFilter(p: AqFilter, document?: string | false) {
     } else {
       if (p.type === 'string' && typeof p.contains === 'string') {
         conditions.push(
-          aql`${literal(path)} ${literal(p.negate ? 'NOT LIKE' : 'LIKE')} '${
+          aql`${literal(path)} ${literal(p.negate ? 'NOT LIKE' : 'LIKE')} ${
             p.contains
-          }'`,
+          }`,
         );
       } else {
         conditions.push(
